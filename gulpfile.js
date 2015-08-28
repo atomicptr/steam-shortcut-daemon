4494bb5779f689c5ae3e70549bf1f71d14adb510
Christopher Kaster <ikasoki@gmail.com>
2015-08-28 16:41:36 +0200

    update to electron 0.31.1

--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,18 @@
 var gulp = require("gulp");
-var electron = require("gulp-electron");
 var run = require("gulp-run-electron");
+var packager = require("electron-packager");
 var windowsInstaller = require("electron-windows-installer");
 var packageJson = require("./app/package.json");
 
 var path = require("path");
 
 var configs = {
-    ELECTRON_VERSION: "v0.31.0",
+    ELECTRON_VERSION: "0.31.1",
     PRODUCT_NAME: packageJson.productName,
     APP_NAME: packageJson.name,
     APP_VERSION: packageJson.version,
     DESCRIPTION: packageJson.description,
-    ICON_URL_PNG: path.resolve(__dirname, "./app/icons/icon.png"),
+    ICON_URL_PNG: path.resolve(__dirname, "app", "icons", "icon.png"),
     AUTHORS: "kasoki"
 };
 
@@ -31,38 +31,52 @@ gulp.task("copy-shortcut-exe", function(folder) {
 });
 
 gulp.task("build", function() {
-    var electronSettings = electron({
-        src: "./app",
-        packageJson: packageJson,
-        release: "./build",
-        cache: "./cache",
+    var packagerOptions = {
+        dir: path.resolve(__dirname, "app"),
+        out: path.resolve(__dirname, "build"),
+        name: configs.APP_NAME,
+        platform: "win32",
+        arch: "x64",
         version: configs.ELECTRON_VERSION,
-        packaging: false,
-        asar: false,
-        platforms: ["win32-x64"],
-        platformResources: {
-            win: {
-                "version-string": configs.APP_VERSION,
-                "file-version": configs.APP_VERSION,
-                "product-version": configs.APP_VERSION,
-                icon: "./app/icons/icon.ico"
-            }
-        }
-    });
+        icon: path.resolve(__dirname, "app", "icons", "icon.ico"),
+        cache: path.resolve(__dirname, "cache"),
+        asar: true,
+        overwrite: true,
+        "app-version": configs.APP_VERSION,
 
-    return gulp.src("").pipe(electronSettings).pipe(gulp.dest(""));
+        "version-string": {
+            CompanyName: configs.AUTHORS,
+            FileDescription: configs.DESCRIPTION,
+            ProductVersion: configs.APP_VERSION,
+            ProductName: configs.PRODUCT_NAME,
+            OriginalFilename: configs.APP_NAME,
+            FileVersion: configs.APP_VERSION,
+            ProductVersion: configs.APP_VERSION,
+            InternalName: configs.APP_NAE,
+            LegalCopyright: "Copyright (C) " + configs.AUTHORS + " 2015."
+        }
+    };
+
+    packager(packagerOptions, function(error, appPath) {
+        if(error) {
+            throw error;
+        }
+
+        var exec = require("child_process").exec;
+        exec("explorer.exe " + appPath);
+    });
 });
 
 gulp.task("package", ["build", "copy-shortcut-exe"]);
 
 gulp.task("build-installer", [], function(done) {
     windowsInstaller({
-        appDirectory: path.resolve(__dirname, "build", configs.ELECTRON_VERSION, "win32-x64"),
+        appDirectory: path.resolve(__dirname, "build", configs.PRODUCT_NAME + "-win32-x64"),
         authors: configs.AUTHORS,
         productName: configs.PRODUCT_NAME,
         description: configs.DESCRIPTION,
         version: configs.APP_VERSION,
         iconUrl: configs.ICON_URL_PNG,
-        setupIcon: path.resolve(__dirname, "./app/icons/icon.ico")
+        setupIcon: path.resolve(__dirname, "app", "icons", "icon.ico")
     });
 });
